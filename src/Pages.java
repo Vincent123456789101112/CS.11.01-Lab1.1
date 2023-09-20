4a8005c7ff5731334e1b8a0de51ee935c9b37aa9
Vincent123456789101112 <145434478+Vincent123456789101112@users.noreply.github.com>
2023-09-20 10:27:59 +0800

    Update Pages.java

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+   {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("Question 2: Pages, Please enter your age: ");
+        int z= scanner.nextInt();
+        System.out.print ("Pages: " + page(z));
     }
+    public static int page (int n) {return (int) ((100-n));}
+
 }
